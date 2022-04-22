982fb7147cdaa2727bcba57092b69e5234f2ca48
carlosdfranca <carlosdudu258@gmail.com>
2022-04-22 12:57:51 -0300

    Separando os arquivos
    Criei o arquivo main, que vai rodar o programa, e deixei as funções no arquivo ContasBancos.py

--- ContasBancos.py
+++ ContasBancos.py
@@ -103,31 +103,3 @@ class CartaoCredito:
             self._senha = valor
         else:
             print('Nova senha INVÁLIDA')
-
-# PROGRAMA
-
-# Conta
-print('='* 10 + ' CONTA ' + '='* 10)
-conta_carlos = ContaCorrente('Carlos', '999.999.999-88', 1234, 29337)
-
-print(f'''
-Nome: {conta_carlos.nome}
-CPF: {conta_carlos.cpf}
-Agência: {conta_carlos.agencia}
-Conta: {conta_carlos.conta}
-''')
-
-# Cartão
-print('='* 10 + ' CARTÃO ' + '='* 10)
-cartao_carlos = CartaoCredito('Fulano Ciclano de Beutrano', conta_carlos)
-
-print(f'''
-Titular: {cartao_carlos.titular}
-Número do cartão : {cartao_carlos.numero}
-Validade: {cartao_carlos.validade}
-Código de segurança: {cartao_carlos.cod_seguranca}
-Conta do cartão: {cartao_carlos.conta_corrente.agencia}-{cartao_carlos.conta_corrente.conta}
-''')
-
-cartao_carlos.senha = '1235'
-print(cartao_carlos.senha)
